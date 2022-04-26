cd0b61faa769717896a3687f348b9852f4f2de0b
전규성 <tjsanf0606@mgail.com>
2022-04-26 13:06:50 +0900

    update next step

--- problem1.py
+++ problem1.py
@@ -10,9 +10,9 @@ import time
 response = requests.get("https://img.freepik.com/free-vector/abstract-distorted-checkered-background_23-2148965764.jpg?t=st=1650245746~exp=1650246346~hmac=29a27d58604684adf6c7a8140834106058f08462598935fa2273b65e54f72461&w=1800")
 img = Image.open(BytesIO(response.content))
 img = img.convert('L')
-img.save("test_img.png")
+img.save("corner_img.png")
 
-img = mpl.pyplot.imread("test_img.png")
+img = mpl.pyplot.imread("corner_img.png")
 
 win_size_list = [3, 5, 7, 9, 11, 15, 21]
 
@@ -32,7 +32,7 @@ def filter(weights,roi):
         for j in range(width,roi.shape[0]-width):
             filtered[j,i] = (weights * roi[j-width:j+width+1, i-height:i+height+1]).sum()       # how do you create the output of the filtering?
     
-    return  filtered.sum()
+    return  filtered
 
 filter_sobelx = np.array([
 [-1,0,1],
@@ -46,22 +46,11 @@ filter_sobely = np.array([
 [-1,-2,-1],
 ])
 
-def intensity(weights,img, window_size):
-    # these two lines are important as they ensure correct
-    # computations!
-    weights = weights.astype(float)    # convert correctly
-    img = img.astype(float)            # convert correctly
-    # this holds the end result
-    filtered = np.zeros_like(img)
-    width = int((window_size-1)/2)
-    height = int((window_size-1)/2)
-    # do the filtering
-    for i in range(height,img.shape[1]-height):
-        for j in range(width,img.shape[0]-width):
-            filtered[j,i] = filter(weights, img[j-width:j+width+1, i-height:i+height+1])
-    
-    return  filtered
+filtered_x = filter(filter_sobelx, img)
+filtered_y = filter(filter_sobely, img)
 
-filtered_x = intensity(filter_sobelx, img, win_size_list[-1])
-filtered_y = intensity(filter_sobely, img, win_size_list[-1])
+window = np.ones((win_size_list[-1], win_size_list[-1]))
 
+hariss_a = filter(window, filtered_x*filtered_x)
+hariss_b = filter(window, filtered_x*filtered_y)
+hariss_c = filter(window, filtered_y*filtered_y)
--- problem2.py
+++ problem2.py
@@ -9,4 +9,14 @@ from PIL import Image
 response = requests.get("https://img1.southernliving.timeinc.net/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2018/07/main/the_sunflower_fields_at_neuse_river_greenway_trail.jpg?itok=ZOlvAuIg&1532035249")
 img = Image.open(BytesIO(response.content))
 img = img.convert('L')
-img.save("test_img2.png")
+img.save("blob_img.png")
+
+def laplacian_of_gaussian(sigma, size):
+    pass
+    
+size = 19
+sigma = 1
+a = np.zeros((size,size))
+for i in range(size):
+    for j in range(size):
+        a[j,i] = -(1 - ((19 - j)**2 + (19 - i)**2) / (2 * sigma**2)) * np.exp
